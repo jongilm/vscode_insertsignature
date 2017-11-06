955427aef18aca8e3d6b41370e32ecdd293599ab
Jonathan Gilmore <jonathan.gilmore@calastone.com>
2017-11-06 14:38:31 +0000

    Add vsix package

---
+++ package_and_install.bat
@@ -0,0 +1,24 @@
+@echo off
+rem -----------------------------------------------------
+rem npm install -g vsce
+rem C:\Users\jonathang\AppData\Roaming\npm\vsce -> C:\Users\jonathang\AppData\Roaming\npm\node_modules\vsce\out\vsce
+rem + vsce@1.32.0
+rem added 61 packages in 4.89s
+rem -----------------------------------------------------
+rem vsce publish
+rem Personal Access Token for publisher 'jongilm':
+rem Error: Invalid Resource
+rem -----------------------------------------------------
+
+vsce package
+echo Expect: Created: c:\home\dev_jg\vscode_insertsignature\jgInsertSignature-0.0.1.vsix
+pause
+
+code --install-extension jgInsertSignature-0.0.1.vsix
+echo Expect: Extension 'jgInsertSignature-0.0.1.vsix' was successfully installed!
+pause
+
+code --list-extensions|grep jg
+echo Expect: jongilm.jgInsertSignature
+pause
+
